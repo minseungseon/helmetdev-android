225d29ba465ad35580f16c2283f1a5d05a33574b
MinseungSeon <seonminseung@naver.com>
2020-11-08 02:12:01 +0900

    :heartbeat: connected signup page and login page!

--- app/src/main/java/com/minseungseon/helmetrev/LoginActivity.java
+++ app/src/main/java/com/minseungseon/helmetrev/LoginActivity.java
@@ -2,13 +2,36 @@ package com.minseungseon.helmetrev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btn_login, btn_register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_register = findViewById( R.id.btn_register );
+        btn_register.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( LoginActivity.this, SignupActivity.class );
+                startActivity( intent );
+            }
+        });
+
+        btn_register = findViewById( R.id.btn_login );
+        btn_register.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( LoginActivity.this, SignupActivity.class );
+                startActivity( intent );
+            }
+        });
     }
 }
