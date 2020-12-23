7c447bcce603bac3097761f887a6ebe57e9f8c50
MinseungSeon <seonminseung@naver.com>
2020-12-24 03:31:13 +0900

    refac: 로그인, 회원가입 부분 프런트 수정, 회원가입 디비 연결, 아직 에러 존재. 블루투스 상시 연결을 위한 작업 중이었음

--- app/src/main/java/com/minseungseon/helmetrev/SignupActivity.java
+++ app/src/main/java/com/minseungseon/helmetrev/SignupActivity.java
@@ -3,12 +3,105 @@ package com.minseungseon.helmetrev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
-
+    private EditText name, id, password, c_password;
+    private Button btn_regist;
+    private ProgressBar loading;
+    private static String URL_REGIST = "http://brain2020.cafe24.com/register.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        loading = findViewById(R.id.loading);
+        name = findViewById(R.id.name);
+        id = findViewById(R.id.id);
+        password=findViewById(R.id.password);
+        c_password=findViewById(R.id.c_password);
+        btn_regist = findViewById(R.id.btn_regist);
+        btn_regist.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Regist();
+            }
+        });
+    }
+
+    private void Regist() {
+        loading.setVisibility(View.VISIBLE);
+        btn_regist.setVisibility((View.GONE));
+
+        final String name = this.name.getText().toString().trim();
+        final String id = this.id.getText().toString().trim();
+        final String password = this.password.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                    try{
+                        JSONObject jsonObject = new JSONObject(response);
+                        String success = jsonObject.getString("success");
+
+                        if(success.equals("1")){
+                            Toast.makeText(SignupActivity.this, "회원가입이 되었습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }catch (JSONException e){
+                        e.printStackTrace();
+                        Toast.makeText(SignupActivity.this, "회원가입에 실패하셨습니다." + e.toString(), Toast.LENGTH_SHORT).show();
+                        loading.setVisibility(View.GONE);
+                        btn_regist.setVisibility(View.VISIBLE);
+
+                    }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(SignupActivity.this, "회원가입에 실패하셨습니다." + error.toString(), Toast.LENGTH_SHORT).show();
+                        loading.setVisibility(View.GONE);
+                        btn_regist.setVisibility(View.VISIBLE);
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("user_name", name);
+                params.put("user_email",id);
+                params.put("user_password",password);
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        stringRequest.setRetryPolicy(new com.android.volley.DefaultRetryPolicy(
+
+                20000 ,
+
+                com.android.volley.DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+
+                com.android.volley.DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue.add(stringRequest);
     }
 }
